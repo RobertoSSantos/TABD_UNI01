7716b4cebff55558c28a17cfa70dc960ac20811c
Pedro <125167776+pedromenezes49@users.noreply.github.com>
2025-04-02 19:18:25 -0300

    Comentários de código

--- OLAP/etl_celular_local.sql
+++ OLAP/etl_celular_local.sql
@@ -1,8 +1,11 @@
 -- ETL tb_celular
 insert into tb_celular (id_celular, modelo, marca) 
 select SQ_celular.nextval, M.DESCRICAO, A.DESCRICAO FROM c##OLTP.modelo M join c##OLTP.marca A on (M.id_marca = A.id_marca);
+-- Pega o modelo e marca do modelo OLTP
+
 
 -- ETL tb_local
 insert into tb_local (id_local, cidade, estado) 
 select SQ_local.nextval, C.DESCRICAO, E.DESCRICAO FROM c##OLTP.cidade C join c##OLTP.estado E on (C.id_estado = E.id_estado)
 where C.descricao not in ('SSA', 'RJ', 'SP');
+--Pega cidade e estado do modelo OLTP, excluindo as cidades duplicatas
